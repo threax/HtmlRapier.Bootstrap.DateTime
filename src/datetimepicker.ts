b212774e2af7beb5bab8c387ce4582804f37471d
Andrew Piper <piper.andrew@spcollege.edu>
2019-02-27 11:10:41 -0500

    Added timezone parsing with moment.js.

--- src/datetimepicker.ts
+++ src/datetimepicker.ts
@@ -5,15 +5,58 @@ import * as controller from 'hr.controller';
 declare var $;
 
 class DateTimeItemEditor extends formbuilder.BasicItemEditor{
+    private displayTimezone: string;
+    private dataTimezone: string;
+
     constructor(args: formbuilder.IFormValueBuilderArgs, format?: string){
         super(args);
         $(this.element).datetimepicker({
             format: format
         });
+
+        var xUi = <any>args.item.xUi;
+        if (xUi && xUi.dataTimezone) {
+            if (moment.tz) {
+                //Schema provided a display timezone
+                if (xUi.displayTimezone) {
+                    this.dataTimezone = xUi.dataTimezone;
+                    this.displayTimezone = xUi.displayTimezone;
+                }
+                //Schema did not provide a timezone, guess the browser's time.
+                else {
+                    this.displayTimezone = moment.tz.guess();
+                    if (this.displayTimezone) {
+                        this.dataTimezone = xUi.dataTimezone;
+                    }
+                    else {
+                        console.warn("Cannot determine browser's timezone. Times will not be localized.");
+                    }
+                }
+            }
+            else {
+                console.warn("The date element specified a timezone, but moment-timezone.js is not loaded. Times will not be localized.");
+            }
+        }
+    }
+
+    public getData(): any {
+        var value = super.getData();
+        if (this.dataTimezone) {
+            moment.tz.setDefault(this.displayTimezone);
+            value = moment(value).tz(this.dataTimezone).format();
+            moment.tz.setDefault();
+        }
+        return value;
     }
 
     public doSetValue(itemData: any) {
         if (itemData !== undefined && itemData !== null) {
+            if (this.dataTimezone) {
+                moment.tz.setDefault(this.dataTimezone);
+                itemData = moment(itemData).tz(this.displayTimezone).format();
+                moment.tz.setDefault();
+            }
+
             $(this.element).data("DateTimePicker").date(new Date(itemData));
         }
         else {
